5a9e000792825cad95c485357905af29161b0c04
PhucDoVan <dvphuc.sgt@gmail.com>
2021-07-17 02:37:06 +0700

    #1  Install Laravel 8 Auth

--- clear-cache.sh
+++ clear-cache.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-docker-compose exec php-server bash -c 'php artisan optimize:clear'
+docker-compose exec php-server bash -c "cd /srv && \
+php composer.phar dump-autoload && \
+php artisan clear-compiled && \
+php artisan optimize && \
+php artisan cache:clear && \
+php artisan config:clear && \
+php artisan config:cache &&\
+rm -rf bootstrap/cache/config.php"
